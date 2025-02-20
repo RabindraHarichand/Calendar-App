f37df5e850ef1e60b7d0469b0ce9272842283856
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-20 17:30:30 -0400

    [Store]: Add Redux for state management; implement uiSlice for date modal control

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export const CalendarApp = () => {
   return (
-    <BrowserRouter
-      future={{
-        v7_relativeSplatPath: true,
-        v7_startTransition: true,
-      }}
-    >
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+          v7_startTransition: true,
+        }}
+      >
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   );
 };
