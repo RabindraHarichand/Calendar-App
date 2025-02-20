d05b7145cc63c5363e4e27e65899d27d640e583e
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-20 15:59:15 -0400

    [CalendarModal]: Add Spanish localization to DatePicker in CalendarModa; enable time selection and update time caption

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,9 +1,13 @@
 import { addHours } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+
+import { es } from "date-fns/locale/es";
+
+registerLocale("es", es);
 
 const customStyles = {
   content: {
@@ -66,6 +70,9 @@ export const CalendarModal = () => {
             onChange={(event) => onDateChanged(event, "start")}
             className="form-control"
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
@@ -77,6 +84,9 @@ export const CalendarModal = () => {
             onChange={(event) => onDateChanged(event, "end")}
             className="form-control"
             dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
           />
         </div>
 
