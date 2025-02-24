8e883ef03b49dfeccd2898db30d8859d025485de
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-24 17:03:49 -0400

    [Auth]: Implement registration functionality in LoginPage with password validation

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -26,6 +26,27 @@ export const useAuthStore = () => {
     }
   };
 
+  // startRegister
+  const startRegister = async ({ name, email, password }) => {
+    console.log({ name, email, password });
+    dispatch(onChecking());
+    try {
+      const { data } = await calendarApi.post("/auth/new", {
+        name,
+        email,
+        password,
+      });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      dispatch(onLogout(error.response.data?.msg || "--"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
+    }
+  };
+
   return {
     //* Propiedades
     status,
@@ -34,5 +55,6 @@ export const useAuthStore = () => {
 
     //* Metodos
     startLogin,
+    startRegister,
   };
 };
