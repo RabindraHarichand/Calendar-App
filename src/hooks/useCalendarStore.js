85efcb8d2181b108ea108e82862ca6bf752a03a2
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-02-25 08:12:41 -0400

    [Events]: Add event loading functionality to calendar store

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   onAddNewEvent,
   onDeleteEvent,
+  onLoadEvents,
   onSetActiveEvent,
   onUpdateEvent,
 } from "../store/calendar/calendarSlice";
@@ -39,7 +40,7 @@ export const useCalendarStore = () => {
     try {
       const { data } = await calendarApi.get("/events");
       const events = convertEventsToDateEvents(data.eventos);
-      console.log(events);
+      dispatch(onLoadEvents(events));
     } catch (error) {
       console.log("Error cargando eventos");
       console.log(error);
