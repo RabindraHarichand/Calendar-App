65eb0044b76273016e24b761e5e29718aae8d081
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-01 17:35:48 -0400

    [Test]: Add tests for authentication flow in AppRouter and update snapshots

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
 import { AppRouter } from "../../src/router/AppRouter";
+import { MemoryRouter } from "react-router-dom";
+import { CalendarPage } from "../../src/calendar";
 
 jest.mock("../../src/hooks/useAuthStore");
+
+jest.mock("../../src/calendar", () => ({
+  CalendarPage: () => <h1>CalendarPage</h1>,
+}));
 
 describe("Pruebas en App router", () => {
   const mockCheckAuthToken = jest.fn();
@@ -18,5 +24,36 @@ describe("Pruebas en App router", () => {
     render(<AppRouter />);
     expect(screen.getByText("Cargando...")).toBeTruthy();
     expect(mockCheckAuthToken).toHaveBeenCalled();
+  });
+
+  test("debe mostrar el login en caso de no estar autenticado", () => {
+    useAuthStore.mockReturnValue({
+      status: "not-authenticated",
+      checkAuthToken: mockCheckAuthToken,
+    });
+
+    const { container } = render(
+      <MemoryRouter initialEntries={["/auth/algo/otracosa"]}>
+        <AppRouter />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Ingreso")).toBeTruthy();
+    expect(container).toMatchSnapshot();
+  });
+
+  test("debe mostrar el calendario si estamos autenticados", () => {
+    useAuthStore.mockReturnValue({
+      status: "authenticated",
+      checkAuthToken: mockCheckAuthToken,
+    });
+
+    render(
+      <MemoryRouter>
+        <AppRouter />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("CalendarPage")).toBeTruthy();
   });
 });
