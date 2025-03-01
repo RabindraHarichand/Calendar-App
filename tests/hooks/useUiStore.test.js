640cf7a490af71f0e1086bc34c272291ee22eed3
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-01 07:42:04 -0400

    [Test]: Add unit tests for useUiStore hook and mock Redux store

---
+++ tests/hooks/useUiStore.test.js
@@ -0,0 +1,35 @@
+import { renderHook } from "@testing-library/react";
+import { useUiStore } from "../../src/hooks/useUiStore";
+import { Provider } from "react-redux";
+import { uiSlice } from "../../src/store";
+import { configureStore } from "@reduxjs/toolkit";
+
+const getMockStore = (initialState) => {
+  return configureStore({
+    reducer: {
+      ui: uiSlice.reducer,
+    },
+    preloadedState: {
+      ui: { ...initialState },
+    },
+  });
+};
+
+describe("Pruebas en el useUiStore", () => {
+  test("Debe regresar los valores por defecto", () => {
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+
+    console.log(result.current);
+    expect(result.current).toEqual({
+      isDateModalOpen: false,
+      openDateModal: expect.any(Function),
+      closeDateModal: expect.any(Function),
+      toggleDateModal: expect.any(Function),
+    });
+  });
+});
